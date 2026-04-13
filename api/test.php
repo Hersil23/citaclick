6d2fe39a14a8d993f336bd8461406ec76bffc448
Hersil23 <herasidesweb@gmail.com>
2026-04-12 22:50:53 -0400

    debug: test.php en routes/ con info del servidor para diagnosticar API

--- api/test.php
+++
@@ -1,4 +0,0 @@
-<?php
-header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-echo json_encode(['status' => 'ok', 'message' => 'API is working', 'php' => PHP_VERSION]);
